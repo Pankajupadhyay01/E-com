ce69a0228b80198b40fdefaa840dd4c784ebe0e2
pankajupadhyay01 <pankajup.git01@gmail.com>
2023-05-15 23:16:48 +0530

    search completed

--- app/search/[name]/page.jsx
+++ app/search/[name]/page.jsx
@@ -1,24 +1,31 @@
 import React from 'react';
 import Card from '@/app/Component/Home/Card';
 async function fetchurl(name) {
-    const res = await fetch(`https://dummyjson.com/products/search?q=${name}`);
-    const result = await res.json();
-    if (result==undefined) {
-        console.log("null");
+    if (name==undefined) {
+        console.log("ok");
     }
-    return result.products;
-   
+    if(name.length==0){
+        alert("ok")
+    }
+    else{
+       const res = await fetch(`https://dummyjson.com/products/search?q=${name}`);
+        const result = await res.json();
+        return result.products; 
+    }  
     
 }
 const page =async ({params:{ name }}) => { 
         const result= await fetchurl(name);  
   return (
     <div>  
+        <div className='flex justify-center m-auto w-[80%] flex-wrap'>
         {
+            
             result.map((pro,i)=>(
                 <Card key={i} title={pro.title} id={pro.id} img={pro?.images[0]} price={pro.price} rating={pro.rating}/>
             ))
-        }     
+        }
+        </div>     
     </div>
   );
 }
